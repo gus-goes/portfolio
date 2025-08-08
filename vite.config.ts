f24bcf50545bbffaa2693e9e964b2fbc4cf07068
Gustavo Goes <gustavorafael2001@gmail.com>
2025-08-08 17:51:15 -0300

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Mantenha apenas UMA chave "plugins"
   plugins: [react()],
+
+  // Suas outras configurações
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
+  base: "/portfolio/",
 });
